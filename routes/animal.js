93e2fd1d783a187a7bf2a507e5ce8f977fbfe533
Nata0524 <141965549+Nata0524@users.noreply.github.com>
2024-04-15 16:22:09 -0500

    clase

---
+++ routes/animal.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router(); //manejador de rutas de express
+const animalSchema = require("../models/animal");
+const verifyToken = require('./validate_token');
+
+//Nuevo animal
+router.post("/animals", (req, res) => {
+    const animal = animalSchema(req.body);
+    animal
+        .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+//Consultar todos los animales
+router.get("/animals", verifyToken, (req, res) => {
+    animalSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+module.exports = router;
